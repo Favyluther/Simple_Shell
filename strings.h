8ef678c477511be4c4b2fabb74f60c26c63c3bee
favyluther <onyemkpachinecherem@gmail.com>
2022-11-10 10:58:53 -0800

    Got Rid Of All Stdlib String Function

---
+++ strings.h
@@ -0,0 +1,11 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+int _strlen(const char *s);
+void _puts(const char *str);
+int _putchar(char c);
+int _strcmp(const char *s1, const char *s2);
+char *_strcat(char *dest, const char *src);
+
+
+#endif
